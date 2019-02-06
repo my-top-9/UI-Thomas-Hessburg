c92a88dc1325a3b057e22ee6b2d45cf566ff6a2b
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-02-06 18:57:12 -0500

    added a project for mock data for the backend

---
+++ seedObjects.js
@@ -0,0 +1,162 @@
+//categories
+let categories = [
+    { name: "beer", description: "hops based alcoholic bevrage",img: "beer.png"},
+    { name: "wine", description: "grape base alcoholic bevrage",img: "wine.png"},
+    { name: "travel", description: "visiting exoctic locations",img: "travel.png"},
+    { name: "lacrosse", description: "traditional sport based on getting a ball in net with sticks",img: "lacrosse.png"},
+    { name: "hockey", description: "sport played on ice with nets",img: "hockey.png"},
+    { name: "soccer", description: "sport played on a field with nets",img: "soccer.png"},
+    { name: "star wars", description: "fictional space based film franchize",img: "starwars.png"},
+    { name: "boxing", description: "combat sport involving strikes with hands",img: "boxing.png"},
+    { name: "Mixed Martial Arts", description: "combat sport involving strikes and grappeling",img: "mma.png"},
+    { name: "wrestling", description: "combat sport involving grappeling and pinning an opponent",img: "wrestling.png"},
+    { name: "ping pong", description: "sport invilving vollying a plastic ball over a net against an opponent",img: "pingpong.png"},
+    { name: "water polo", description: "sport involving throwing a ball into a net while swimming",img: "waterpolo.png"},
+    { name: "bowling", description: "sport involving knocking down pins with a weighted ball",img: "bowling.png"},
+    { name: "tennis", description: "sport invilving vollying a ball over a net against an opponent",img: "tennis.png"},
+    { name: "fortnite", description: "first person shooter video game",img: "fornite.png"},
+    { name: "call of duty", description: "first person shooter video game",img: "callofduty.png"},
+    { name: "halo", description: "first person shooter video game",img: "halo.png"},
+    { name: "world of warcraft", description: "online role playing game",img: "wow.png"},
+    { name: "runescape", description: "online role playing game",img: "runescape.png"},
+    { name: "minecraft", description: "online video game",img: "minecraft.png"},
+    { name: "tetris", description: "old school puzzle style video game",img: "tetris.png"}
+]
+
+
+
+
+//accounts
+
+let accounts = [
+    {
+    username: "thomasGH", 
+    password: password,
+    rank1: "1",
+    rank2: "4",
+    rank3: "7",
+    rank4: "19",
+    rank5: "3",
+    rank6: "42",
+    rank7: "17",
+    rank8: "18",
+    rank9: "28"
+    },
+    {
+    username: "sally123", 
+    password: password,
+    rank1: "5",
+    rank2: "9",
+    rank3: "14",
+    rank4: "19",
+    rank5: "22",
+    rank6: "28",
+    rank7: "31",
+    rank8: "37",
+    rank9: "44"
+    },
+    {
+    username: "xKillian", 
+    password: password,
+    rank1: "2",
+    rank2: "14",
+    rank3: "15",
+    rank4: "17",
+    rank5: "21",
+    rank6: "24",
+    rank7: "36",
+    rank8: "39",
+    rank9: "45"
+    },
+    {
+    username: "xKillian", 
+    password: password,
+    rank1: "1",
+    rank2: "24",
+    rank3: "25",
+    rank4: "37",
+    rank5: "31",
+    rank6: "34",
+    rank7: "46",
+    rank8: "49",
+    rank9: "15"
+    },
+    {
+    username: "billyNYE", 
+    password: password,
+    rank1: "9",
+    rank2: "11",
+    rank3: "14",
+    rank4: "16",
+    rank5: "24",
+    rank6: "26",
+    rank7: "36",
+    rank8: "43",
+    rank9: "48"
+    },
+    {
+    username: "scienceGUY", 
+    password: password,
+    rank1: "4",
+    rank2: "14",
+    rank3: "16",
+    rank4: "17",
+    rank5: "26",
+    rank6: "25",
+    rank7: "23",
+    rank8: "41",
+    rank9: "43"
+    },
+    {
+    username: "gurtrudethecat", 
+    password: password,
+    rank1: "6",
+    rank2: "16",
+    rank3: "19",
+    rank4: "22",
+    rank5: "24",
+    rank6: "29",
+    rank7: "31",
+    rank8: "38",
+    rank9: "47"
+    },
+    {
+    username: "baronFIELDS", 
+    password: password,
+    rank1: "2",
+    rank2: "14",
+    rank3: "22",
+    rank4: "29",
+    rank5: "31",
+    rank6: "36",
+    rank7: "38",
+    rank8: "44",
+    rank9: "48"
+    },
+    {
+    username: "ethanK", 
+    password: password,
+    rank1: "1",
+    rank2: "4",
+    rank3: "9",
+    rank4: "16",
+    rank5: "14",
+    rank6: "27",
+    rank7: "32",
+    rank8: "35",
+    rank9: "39"
+    },
+    {
+    username: "hilaK", 
+    password: password,
+    rank1: "3",
+    rank2: "4",
+    rank3: "19",
+    rank4: "22",
+    rank5: "25",
+    rank6: "27",
+    rank7: "33",
+    rank8: "36",
+    rank9: "40"
+    },
+]
